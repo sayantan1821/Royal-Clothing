ced67f204b3e0eec22b62ec7fcc6acac3075f249
Sayantan Kapat <ksayantan1823@gmail.com>
2021-03-12 22:38:10 +0530

    navigation added and menu items updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import HomePage from './component/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+
+import { BrowserRouter, Switch, Route} from 'react-router-dom';    
+
+
+const HatsPage = () => (
+  <div>
+    <h1>
+      Hats Page
+    </h1>
+  </div>
+);
 class App extends Component {
   render() {
     return <div className='App'>
-      <HomePage />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component= {HomePage} />
+        <Route path='/shop/hats' component= {HatsPage} />
+      </Switch>
+    </BrowserRouter>
     </div>;
   }
 }
