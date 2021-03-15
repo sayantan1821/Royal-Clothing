93848cd8e4e85b1ff70c7599bdeaad59c12ec492
Sayantan Kapat <ksayantan1823@gmail.com>
2021-03-15 19:59:35 +0530

    header component added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom'; 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './component/header/header.component';
 
-import { BrowserRouter, Switch, Route} from 'react-router-dom';    
+   
 
 
 class App extends Component {
   render() {
     return <div className='App'>
+    
     <BrowserRouter>
+    <Header />
       <Switch>
         <Route exact path='/' component= {HomePage} />
         <Route exact path='/shop' component= {ShopPage} />
