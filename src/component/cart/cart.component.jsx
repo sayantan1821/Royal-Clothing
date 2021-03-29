14494a28aaa2c84cd012ac67265e71152a041c8b
Sayantan Kapat <ksayantan1823@gmail.com>
2021-03-29 18:35:41 +0530

    checkout page bug fixed

--- src/component/cart/cart.component.jsx
+++ src/component/cart/cart.component.jsx
@@ -1,28 +1,34 @@
-  
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
+
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
-import {selectCartItems} from '../../redux/cart/cart.selector';
-import { withRouter } from 'react-router-dom';
+import { selectCartItems } from '../../redux/cart/cart.selector';
+import { toggleCartHidden } from '../../redux/cart/cart.action.js';
+
 import './cart.style.scss';
 
-const Cart = ({ cartItems, history }) => (
+const Cart = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
-    {
-      cartItems.length ? (
+      {cartItems.length ? (
         cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
       ) : (
-        <span className='empty-message'>Cart is empty</span>
-      )
-    }
-      {}
+        <span className='empty-message'>Your cart is empty</span>
+      )}
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>CHECKOUT</CustomButton>
+    <CustomButton
+      onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden());
+      }}
+    >
+      GO TO CHECKOUT
+    </CustomButton>
   </div>
 );
 
