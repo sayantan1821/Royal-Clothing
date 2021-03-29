ae09a293ac53f99c238a3880d2b63d2178672d06
Sayantan Kapat <ksayantan1823@gmail.com>
2021-03-29 20:18:32 +0530

    item remeove , quantity increase and decrease features added

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.type';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, RemoveItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -17,6 +17,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+
+      case CartActionTypes.REMOVE_ITEM:
+        return {
+          ...state,
+          cartItems: RemoveItemFromCart(state.cartItems, action.payload)
+        }
+
+    case CartActionTypes.CLEAR_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id) 
       };
     default:
       return state;
