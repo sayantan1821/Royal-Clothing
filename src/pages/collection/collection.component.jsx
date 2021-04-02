20483cd1078a9c3e21c4147041e5ed284d1f7f57
Sayantan Kapat <ksayantan1823@gmail.com>
2021-04-02 21:31:07 +0530

    category page created

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CollectionItems from '../../component/collection-item/collection-item.component';
+const CollectionPage = ({match}) =>(
+    <div className= 'collection-page'>
+        <h1>ok</h1>
+    </div>
+);
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { Route } from 'react-router-dom';
 import CollectionOverview from '../../component/collections-overview/collection-overview.component';
-
-const ShopPage = ({ collections }) => (
+import CollectionPage from '../collection/collection.component';
+const ShopPage = ({ match }) => (
   <div className= 'shop-page'>
-    <CollectionOverview />
+    <Route exact path={`${match.path}`} component= {CollectionOverview} />
+    <Route exact path={`${match.path}/:collectionId`} component= {CollectionPage} />
   </div>
 );
 export default ShopPage;
