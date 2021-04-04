c8239ffec95ff09e7fa2a1fd48929adb0862908c
Sayantan Kapat <ksayantan1823@gmail.com>
2021-04-04 23:38:50 +0530

    shop page error fixed

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-// import CollectionItems from '../../component/collection-item/collection-item.component';
+import CollectionItems from '../../component/collection-item/collection-item.component';
 import { selectCollection } from '../../redux/shop/shop.selector';
-const CollectionPage = ({match}) =>(
-    <div className= 'collection-page'>
-        <h1>ok</h1>
-    </div>
-);
+const CollectionPage = ({match}) =>{
+    console.log(match.params.collectionId);
+    return(
+        <div className= 'collection-page'>
+            <h1>ok</h1>
+        </div>
+    );
+};
 const mapStateToProps = (state, ownProps) => ({
-    collection: selectCollection(ownProps.match.params.collection.Id)(state)
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
 })
 export default connect(mapStateToProps)(CollectionPage);
